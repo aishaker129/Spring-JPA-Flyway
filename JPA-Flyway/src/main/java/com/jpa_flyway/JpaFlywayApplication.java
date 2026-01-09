6fbe2288a553e632b1576f62c04b91e497866555
aishaker129 <aishaker129@gmail.com>
2026-01-10 00:07:54 +0600

    initiate new spring boot jap flyway project configuration

---
+++ JPA-Flyway/src/main/java/com/jpa_flyway/JpaFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.jpa_flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFlywayApplication.class, args);
+	}
+
+}
---
+++ JPA-Flyway/src/test/java/com/jpa_flyway/JpaFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa_flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
